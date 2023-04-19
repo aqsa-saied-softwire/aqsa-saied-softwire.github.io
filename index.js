237a29b29b835354d027810e8170f1967baa0160
aqsa-saied-softwire <aqsa.saied@softwire.com>
2023-04-19 16:28:59 +0100

    initial styling

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function showMore() {
     var list = document.getElementById("list");
-    list.innerHTML += '<li id="addedLine"> Bury - Piccadilly </li>';
-    const addedLine = document.getElementById("addedLine");
-    addedLine.className = "light-blue";
+    list.innerHTML += '<li class="light-blue"> Bury - Piccadilly </li>';
 }
