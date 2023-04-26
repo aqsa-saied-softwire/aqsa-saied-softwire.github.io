5733728a92c6d28b0f20398b8c0a020ad3f57b85
aqsa-saied-softwire <aqsa.saied@softwire.com>
2023-04-26 15:34:39 +0100

    no more infinite burys

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const htmls = ['<li class="yellow"> Bury - Piccadilly </li>', '<li class="grey"> East Didsbury - Shaw and Crompton </li>', '<li class="pink"> East Didsbury - Rochdale Town Centre </li>']
+
+let x = 0;
 function showMore() {
     var list = document.getElementById("list");
-    list.innerHTML += '<li class="light-blue"> Bury - Piccadilly </li>';
+    if (x < htmls.length) {
+        list.innerHTML += htmls[x];
+        x = x + 1;
+    }
 }
